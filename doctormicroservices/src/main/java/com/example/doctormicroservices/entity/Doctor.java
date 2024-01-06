b608353ee2afc9497985f7b364fd636572e442d7
vigne <vigne@MadaraUchiha>
2024-01-06 14:46:29 +0530

    doctor commit

---
+++ doctormicroservices/src/main/java/com/example/doctormicroservices/entity/Doctor.java
@@ -0,0 +1,45 @@
+package com.example.doctormicroservices.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Doctor {
+
+	@Id
+	private String doctorName;
+	private String doctorQualification;
+	private String doctorSpecialist;
+	private String doctorEmail;
+	private String doctorContactNo;
+
+	public String getDoctorName() {
+		return doctorName;
+	}
+
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+
+	public String getDoctorSpecialist() {
+		return doctorSpecialist;
+	}
+
+	public void setDoctorSpecialist(String doctorSpecialist) {
+		this.doctorSpecialist = doctorSpecialist;
+	}
+
+	
+}
