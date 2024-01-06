2efd7a34c7afed408118da8487a43388bf15a183
vigne <vigne@MadaraUchiha>
2024-01-06 14:50:48 +0530

    appointment comit

---
+++ appointmentmicroservice/src/main/java/com/example/appointmentmicroservices/entity/Appointment.java
@@ -0,0 +1,97 @@
+package com.example.appointmentmicroservices.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Appointment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int appointmentId;
+	private String patientName;
+	private String doctorName;
+	private LocalDate dateOfAppointment;
+	private String timeSlot;
+	private String status;
+	private String remark;
+	private int age;
+	private String gender;
+	private long contactNo;
+
+	public int getAppointmentId() {
+		return appointmentId;
+	}
+	public void setAppointmentId(int appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+	public String getPatientName() {
+		return patientName;
+	}
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+	public String getDoctorName() {
+		return doctorName;
+	}
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+	public LocalDate getDateOfAppointment() {
+		return dateOfAppointment;
+	}
+	public void setDateOfAppointment(LocalDate dateOfAppointment) {
+		this.dateOfAppointment = dateOfAppointment;
+	}
+	public String getTimeSlot() {
+		return timeSlot;
+	}
+	public void setTimeSlot(String timeSlot) {
+		this.timeSlot = timeSlot;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public long getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(long contactNo) {
+		this.contactNo = contactNo;
+	}
+
+}
---
+++ appointmentmicroservice/src/main/java/com/example/appointmentmicroservices/model/Patient.java
@@ -0,0 +1,92 @@
+package com.example.appointmentmicroservices.model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+	private String patientName;
+	private int age;
+	private String gender;
+	private long contactNo;
+	private String doctorId;
+	private String specialization;
+	private LocalDate dateOfAppointment;
+	private String timeSlot;
+
+	public String getPatientName() {
+		return patientName;
+	}
+
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public long getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(long contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getDoctorId() {
+		return doctorId;
+	}
+
+	public void setDoctorId(String doctorId) {
+		this.doctorId = doctorId;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	public LocalDate getDateOfAppointment() {
+		return dateOfAppointment;
+	}
+
+	public void setDateOfAppointment(LocalDate dateOfAppointment) {
+		this.dateOfAppointment = dateOfAppointment;
+	}
+
+	public String getTimeSlot() {
+		return timeSlot;
+	}
+
+	public void setTimeSlot(String timeSlot) {
+		this.timeSlot = timeSlot;
+	}
+
+}
